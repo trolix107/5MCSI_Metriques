5245339ffedbd38ac1967b7a344e3f9856f103f0
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-04-01 14:32:44 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
+from datetime import datetime
+from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
+    return render_template('hello.html')
 
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
-                                                                                                               
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
+  
 if __name__ == "__main__":
   app.run(debug=True)
