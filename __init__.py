71623317878e638727f2460e907483e61999c779
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-04-01 14:37:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-@app.route("/histogramme/")
+@app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
 
