298985d6decef3d08874e359d13924ce8e67601f
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-04-01 14:39:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,6 +31,8 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-
+@app.route("/histogrammes/")
+def mongraphique():
+    return render_template("histogramme.html")
 if __name__ == "__main__":
   app.run(debug=True) 
