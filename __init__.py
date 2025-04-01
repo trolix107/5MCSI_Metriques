f013032a0196a748d3760ec79829722d27a38aff
trolix107 <gregoire.barix@edu.esiee-it.fr>
2025-04-01 14:55:05 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,8 @@ def hello_world():
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return "<h2>Ma page de contact allooooooo
+  </h2>"
 
 @app.route('/tawarano/')
 def meteo():
